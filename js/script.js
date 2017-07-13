500114e5a8f2d8ad9274d46ab46469c4f4652496
Akachi Ogbonna <akachinonyerem.o@gmail.com>
2017-07-13 15:47:27 +0100

    Six commit?
    sign up for beta modal.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
 $(document).ready(function() {
-	
+
     $('.toggle-nav').click(function(e) {
         $(this).toggleClass('active');
         $('.nav-menu ul').toggleClass('active');
 
         e.preventDefault();
     });
+
+    var modal = document.getElementById('beta-modal'); //modal
+
+    //button
+    var btn = document.getElementById('beta-btn');
+
+    //close 
+    var span = document.getElementsByClassName('close')[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "flex";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
 });
